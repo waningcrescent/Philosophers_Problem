eb5ebd44a74f59cc538b03f8c94b6612b7c7ebc6
Shriya Verma <104719408+waningcrescent@users.noreply.github.com>
2022-12-20 19:50:55 +0530

    Update PhilosopherB1.c

--- PhilosopherB1.c
+++ PhilosopherB1.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
 #include<semaphore.h>
 #include<stdbool.h>
 #include<pthread.h>
+#include<stdlib.h>
 
+
+sem_t BOWL;
 sem_t fork_[5];
-sem_t bowl;
+
 
 void attempt_right_fork(int num){
 
@@ -33,7 +35,9 @@ void attempt_left_fork(int num){
 void eating(int num){
     printf("Philosopher number %d is eating right now",num);    
     printf("\n");
-
+    sleep(2);
+    printf("Philosopher number %d is done eating",PhilosopherNum);
+    printf("\n");
 }
 
 
@@ -56,27 +60,22 @@ void * let_go_fork(void * b){
             attempt_right_fork(PhilosopherNum);
         }
 
-        sem_getvalue(&bowl,&m);
+        sem_getvalue(&BOWL,&m);
         printf("\n");
         printf("Bowl Semaphore is : %d",m);
         printf("\n");
 
-        sem_wait(&bowl);
+        sem_wait(&BOWL);
         printf("Philosopher number %d has taken bowl and will start eating",PhilosopherNum);
         printf("\n");
 
         eating(PhilosopherNum);
-        sleep(2);
 
-        printf("Philosopher number %d is done eating",PhilosopherNum);
-        printf("\n");
-
-
-        sem_post(&bowl);
+        sem_post(&BOWL);
         printf("Philosopher %d has returned the bowl to table",PhilosopherNum);
         printf("\n");
 
-        sem_getvalue(&bowl,&m);
+        sem_getvalue(&BOWL,&m);
         printf("\n");
         printf("Bowl Semaphore is : %d",m);
         printf("\n");
@@ -84,6 +83,7 @@ void * let_go_fork(void * b){
         sem_post(&fork_[(PhilosopherNum+1)%5]);
         printf("Philosopher number %d returns left fork to table",PhilosopherNum);
         printf("\n");
+        
         sem_post(&fork_[(PhilosopherNum)]);
         printf("Philosopher number %d returns left fork to table",PhilosopherNum);
         printf("\n");
@@ -94,27 +94,27 @@ void * let_go_fork(void * b){
 }
 
 int main(){
-
   
     int threadnum[5];
     pthread_t thr[5];
-
-    int x = 0;
+    
     int j = 0;
     while(j<5){
         sem_init(&fork_[j],0,1);
         j++;
     }
     
-    sem_init(&bowl,0,2);
-
-    for(int i = 0 ; i<5 ; i++){
-
-        threadnum[i]=i;
-        pthread_create(&thr[i],NULL,let_go_fork,(void *)&threadnum[i]);
+    sem_init(&BOWL,0,2);
+    
+    int y = 0;
+    while(y<=4){
+        threadnum[y]=y;
+        pthread_create(&thr[y],NULL,let_go_fork,(void *)&threadnum[y]);
+        y++;
 
     }
-
+    
+   int x = 0;
    while(x<5){
         pthread_join(thr[x],NULL);
         x++;
