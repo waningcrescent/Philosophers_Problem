9edc46a2b38f6eef9c45d36a6ba552cdb7b040a5
Shriya Verma <104719408+waningcrescent@users.noreply.github.com>
2022-12-20 20:02:18 +0530

    Update PhilosopherB1.c

--- PhilosopherB1.c
+++ PhilosopherB1.c
@@ -1,97 +1,14 @@
+#include<pthread.h>
 #include<stdio.h>
 #include<unistd.h>
 #include<semaphore.h>
 #include<stdbool.h>
-#include<pthread.h>
 #include<stdlib.h>
 
 
 sem_t BOWL;
 sem_t fork_[5];
 
-
-void attempt_right_fork(int num){
-
-    printf("Philosopher number %d tries to pick right fork up",num);
-    printf("\n");
-    sem_wait(&fork_[(num+1)%5]);
-    printf("Philosopher number %d picks the right fork up",num);
-    printf("\n");
-
-
-}
-
-void attempt_left_fork(int num){
-
-    printf("Philosopher number %d tries to pick left fork up",num);    
-    printf("\n");
-
-    sem_wait(&fork_[num]);
-    printf("Philosopher number %d picks the left fork up",num);
-    printf("\n");
-
-}
-
-void eating(int num){
-    printf("Philosopher number %d is eating right now",num);    
-    printf("\n");
-    sleep(2);
-    printf("Philosopher number %d is done eating",num);
-    printf("\n");
-}
-
-
-
-void * let_go_fork(void * b){
-
-    while (true)
-    {
-        int PhilosopherNum=*(int *)b;
-        int m;
-
-        if(PhilosopherNum==4){
-
-            attempt_right_fork(4);
-            attempt_left_fork(4);
-
-        }else{
-
-            attempt_left_fork(PhilosopherNum);
-            attempt_right_fork(PhilosopherNum);
-        }
-
-        sem_getvalue(&BOWL,&m);
-        printf("\n");
-        printf("Bowl Semaphore is : %d",m);
-        printf("\n");
-
-        sem_wait(&BOWL);
-        printf("Philosopher number %d has taken bowl and will start eating",PhilosopherNum);
-        printf("\n");
-
-        eating(PhilosopherNum);
-
-        sem_post(&BOWL);
-        printf("Philosopher %d has returned the bowl to table",PhilosopherNum);
-        printf("\n");
-
-        sem_getvalue(&BOWL,&m);
-        printf("\n");
-        printf("Bowl Semaphore is : %d",m);
-        printf("\n");
-
-        sem_post(&fork_[(PhilosopherNum+1)%5]);
-        printf("Philosopher number %d returns left fork to table",PhilosopherNum);
-        printf("\n");
-        
-        sem_post(&fork_[(PhilosopherNum)]);
-        printf("Philosopher number %d returns left fork to table",PhilosopherNum);
-        printf("\n");
-        
-
-    }
-     
-}
 
 int main(){
   
@@ -111,7 +28,6 @@ int main(){
         threadnum[y]=y;
         pthread_create(&thr[y],NULL,let_go_fork,(void *)&threadnum[y]);
         y++;
-
     }
     
    int x = 0;
@@ -121,4 +37,88 @@ int main(){
     }
 
     return 0;
+}
+
+
+void attempt_right_fork(int num){
+
+    printf("Philosopher number %d tries to pick right fork up",num);
+    printf("\n");
+    sem_wait(&fork_[num]);
+    printf("Philosopher number %d picks the right fork up",num);
+    printf("\n");
+
+
+}
+
+void attempt_left_fork(int num){
+
+    printf("Philosopher number %d tries to pick left fork up",num);    
+    printf("\n");
+    sem_wait(&fork_[(num+1)%5]);
+    printf("Philosopher number %d picks the left fork up",num);
+    printf("\n");
+
+}
+
+void eating(int num){
+    printf("Philosopher number %d is eating right now",num);    
+    printf("\n");
+    sleep(2);
+    printf("Philosopher number %d is done eating",num);
+    printf("\n");
+}
+
+
+
+void * let_go_fork(void * b){
+
+    while (true)
+    {
+        long m;
+        long PhilosopherNum=*(int *)b;
+
+        if(PhilosopherNum==4){
+
+            attempt_left_fork(4);
+            attempt_right_fork(4);
+
+        }else{
+
+            attempt_right_fork(PhilosopherNum);
+            attempt_left_fork(PhilosopherNum);
+        }
+
+        sem_getvalue(&BOWL,&m);
+        printf("\n");
+        printf("Bowl Semaphore is : %d",m);
+        printf("\n");
+
+        sem_wait(&BOWL);
+        printf("Philosopher number %d has taken bowl and will start eating",PhilosopherNum);
+        printf("\n");
+
+        eating(PhilosopherNum);
+
+        sem_post(&BOWL);
+        printf("Philosopher %d has returned the bowl to table",PhilosopherNum);
+        printf("\n");
+
+        sem_post(&fork_[(PhilosopherNum+1)%5]);
+        printf("Philosopher number %d returns left fork to table",PhilosopherNum);
+        printf("\n");
+        
+        sem_post(&fork_[(PhilosopherNum)]);
+        printf("Philosopher number %d returns right fork to table",PhilosopherNum);
+        printf("\n");
+        
+          sem_getvalue(&BOWL,&m);
+        printf("\n");
+        printf("Bowl Semaphore is : %d",m);
+        printf("\n");
+
+        
+
+    }
+     
 }
